3b6abc7cd1ed0ffe4290f1f55dd93380bc114c31
Mannan <Mannan@DESKTOP-OTIQOF5>
2024-07-09 23:13:01 +0530

    working build for google forms

--- background.js
+++ background.js
@@ -1,17 +1,4 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getAnswer") {
-      fetch('YOUR_CHATGPT_API_ENDPOINT', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer YOUR_API_KEY`
-        },
-        body: JSON.stringify({ question: request.question })
-      })
-      .then(response => response.json())
-      .then(data => sendResponse({ answer: data.answer }))
-      .catch(error => sendResponse({ error: error.toString() }));
-      return true; // Indicates that the response will be sent asynchronously
-    }
-  });
-  
+// background.js
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Google Form Auto Answer extension installed');
+});
